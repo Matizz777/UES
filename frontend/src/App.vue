155dd365a781bffb2e06dfb88a0ea1555fdfef69
Matizz777 <Matizz777>
2026-05-05 19:22:35 +0300

    Veidota sākumlapa projektam

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,23 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="navbar">
+    <div class="logo">UES</div>
+    <div class="auth-buttons">
+      <button class="btn-login">Pieslēgties</button>
+      <button class="btn-register">Reģistrēties</button>
     </div>
-  </header>
-
-  <RouterView />
+  </nav>
+<div class="apraksts">
+  <main class="hero">
+    <h1>Universālā e-pierakstu sistēma</h1>
+    <p>Pieraksts pāris klikšķu attālumā</p>
+  </main>
+</div>
+<div class="clouds-container">
+  <div class="cloud cloud-solid"></div>
+  <div class="cloud cloud-transparent"></div>
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script setup>
+import './assets/style.css';
+</script>
