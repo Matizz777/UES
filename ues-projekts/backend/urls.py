00c6f009a3eae896987510bff64f355cd0c2be8d
Matizz777 <Matizz777>
2026-05-06 19:53:29 +0300

    reģistrācija, sessija

--- ues-projekts/backend/urls.py
+++ ues-projekts/backend/urls.py
@@ -1,22 +1,51 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password
+from django.db import connection, IntegrityError
+import json
+import uuid
+
+@csrf_exempt
+def register_user(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            email = data.get('email')
+            password = make_password(data.get('password'))
+            role = int(data.get('roles', 3))
+            
+            session_token = str(uuid.uuid4()) 
+
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO auth_user (
+                        username, email, password, roles, 
+                        is_active, date_joined, first_name, last_name,
+                        session_token
+                    )
+                    VALUES (%s, %s, %s, %s, %s, NOW(), %s, %s, %s)
+                    """,
+                    [username, email, password, role, True, '', '', session_token]
+                )
+
+            return JsonResponse({
+                "token": session_token,
+                "username": username,
+                "roles": role
+            }, status=201)
+            
+        except IntegrityError:
+            return JsonResponse({"error": "Lietotājvārds jau ir aizņemts!"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"status": "Online"}, status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', register_user, name='register'),
 ]
