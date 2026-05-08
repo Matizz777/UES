b3975f59c74dfd40ba1436b014a7e57fa170b490
Matizz777 <Matizz777>
2026-05-08 22:42:14 +0300

    rezervācija, pakalpojumu pievienošana, atcelšana

--- ues-projekts/backend/urls.py
+++ ues-projekts/backend/urls.py
@@ -4,48 +4,20 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from django.db import connection, IntegrityError
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
 import json
 import uuid
-
-@csrf_exempt
-def register_user(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            username = data.get('username')
-            email = data.get('email')
-            password = make_password(data.get('password'))
-            role = int(data.get('roles', 3))
-            
-            session_token = str(uuid.uuid4()) 
-
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    """
-                    INSERT INTO auth_user (
-                        username, email, password, roles, 
-                        is_active, date_joined, first_name, last_name,
-                        session_token
-                    )
-                    VALUES (%s, %s, %s, %s, %s, NOW(), %s, %s, %s)
-                    """,
-                    [username, email, password, role, True, '', '', session_token]
-                )
-
-            return JsonResponse({
-                "token": session_token,
-                "username": username,
-                "roles": role
-            }, status=201)
-            
-        except IntegrityError:
-            return JsonResponse({"error": "Lietotājvārds jau ir aizņemts!"}, status=400)
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
-
-    return JsonResponse({"status": "Online"}, status=200)
+from . import views
+from datetime import datetime, timedelta
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', register_user, name='register'),
+    path('api/register/', views.register_user, name='register'),
+    path('api/my-reservations/', views.get_user_reservations),
+    path('api/book/', views.save_booking, name='book'),
+    path('api/occupied-times/', views.get_occupied_times, name='occupied_times'),
+    path('api/cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('api/services/add/', views.add_service, name='add_service'),
+    path('api/catalog/', views.get_providers, name='get_providers'),
 ]
